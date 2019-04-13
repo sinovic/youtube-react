5c0377afe9fa1244baab6d6712e35808c6889bb2
Yassine Meftah <meftah.champart@gmail.com>
2019-04-13 19:35:05 +0100

    Create header navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        Hello, World!
+        <HeaderNav/>
       </React.Fragment>
     );
   }
