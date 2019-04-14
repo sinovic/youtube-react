a52ab1d90a8d43c8f17aec927f4ad50ee1107474
Yassine Meftah <meftah.champart@gmail.com>
2019-04-14 11:51:45 +0100

    build sticky sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import HeaderNav from './containers/HeaderNav/HeaderNav'
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+import SideBar from './containers/SideBar/SideBar';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <HeaderNav/>
+        <SideBar/>
       </React.Fragment>
     );
   }
